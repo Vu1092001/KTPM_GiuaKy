9a4313fe6b8ad0b8ce01cab5ea489ff43cb3fed1
Vu1092001 <112168479+Vu1092001@users.noreply.github.com>
2022-10-21 10:23:32 +0700

    19432571_HoangHuyVu

---
+++ ThiGK/src/test/java/com/example/ThiGK/ThiGkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThiGK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThiGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
